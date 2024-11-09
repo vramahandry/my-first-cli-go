9a7f0187b01587a13284a773b984b2eb97bab162
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-11-09 14:06:31 +0100

    feat(chapter5): init my first cli with cobra

---
+++ chapter5/README.md
@@ -0,0 +1,3 @@
+## My First Cli in GO
+Thanks to this [article](https://dev.to/aurelievache/learning-go-by-examples-part-3-create-a-cli-app-in-go-1h43)
+ from scraly
---
+++ chapter5/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/vramahandry/everyday-go/chapter5/my-first-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
