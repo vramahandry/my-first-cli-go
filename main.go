225ec6601e1fcd820db5f2b2ede5b97c5043b867
vramahandry <56304555+vramahandry@users.noreply.github.com>
2024-12-22 23:01:46 +0100

    chore: change repo everyday-go to my-first-cli-go

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/vramahandry/everyday-go/chapter5/my-first-cli/cmd"
+import "github.com/vramahandry/my-first-cli/cmd"
 
 func main() {
 	cmd.Execute()
